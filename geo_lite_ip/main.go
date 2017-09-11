3498f4ce4cbbf9dead5c7742a783ee837a683a08
felce <zabara.alexey@gmail.com>
2017-09-11 15:40:08 +0300

    change

--- geo_lite_ip/main.go
+++ geo_lite_ip/main.go
@@ -41,6 +41,7 @@ func getIP(w http.ResponseWriter, r *http.Request, data *libgeo.GeoIP) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var ip string
+	var infoJson []byte
 	if ipProxy := r.Header.Get("X-FORWARDED-FOR"); len(ipProxy) > 0 {
 		ip = ipProxy
 	} else {
@@ -52,39 +53,29 @@ func getIP(w http.ResponseWriter, r *http.Request, data *libgeo.GeoIP) {
 	if clientInfo != nil {
 
 		w.WriteHeader(http.StatusOK)
-		infoJson, _ := json.MarshalIndent(clientInfo, "", "\t")
-		w.Write(infoJson)
+		infoJson, _ = json.MarshalIndent(clientInfo, "", "\t")
 	} else {
 
-		errInfo := &ErrorInfo{}
-		errInfo.Error = "geo info for ip not found"
-		errInfo.Ip = ip
 		w.WriteHeader(http.StatusNotFound)
-		infoJson, _ := json.MarshalIndent(errInfo, "", "\t")
-		w.Write(infoJson)
+		errInfo := &ErrorInfo{ip, "geo info for ip not found"}
+		infoJson, _ = json.MarshalIndent(errInfo, "", "\t")
 	}
+	w.Write(infoJson)
 }
 
 func ipInfo(ipAddr string, data *libgeo.GeoIP) *ClientInfo {
 
-	clientInfo := &ClientInfo{}
-	clientInfo.Ip = ipAddr
 	mutex.Lock()
 	defer mutex.Unlock()
 
 	loc := data.GetLocationByIP(ipAddr)
-	if loc != nil {
-
-		clientInfo.Country = loc.CountryName
-		clientInfo.CountryCode = loc.CountryCode
-		clientInfo.City = loc.City
-		clientInfo.Region = loc.Region
-		clientInfo.PostalCode = loc.PostalCode
-		clientInfo.Latitude = loc.Latitude
-		clientInfo.Longitude = loc.Longitude
-		return clientInfo
+	if loc == nil {
+		return nil
 	}
-	return nil
+	clientInfo := &ClientInfo{ipAddr, loc.CountryName, loc.CountryCode,
+		loc.City, loc.Region, loc.PostalCode, loc.Latitude, loc.Longitude}
+
+	return clientInfo
 }
 
 func main() {
