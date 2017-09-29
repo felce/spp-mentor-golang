835259dd19eb85ce4ade9df865611fa7d9b87b53
felce <zabara.alexey@gmail.com>
2017-09-29 22:19:45 +0300

    add auth

--- sockets/asset/script.js
+++ sockets/asset/script.js
@@ -1,14 +1,44 @@
+var ifAuth = false;
 var socket = io();
 var d = new Date();
+var usersConnect = [];
+
+var text = "";
+var name = "";
 
 var id = d.getTime().toString();
+var nameId = (d.getTime() * 2).toString();
+
 clientCursorColor = "#" + Math.floor(Math.random() * 700000 + 100000);
 
-$("#field").mousemove(function (event) {
-  var coordsX = event.pageX + "";
-  var coordsY = event.pageY + "";
+$('#btn').click(function () {
+  ifAuth = true;
+  name = $('#nameInput').val() + "";
+  socket.emit('user', name, clientCursorColor, nameId, id);
 
-  socket.emit('coords', coordsX, coordsY, clientCursorColor, id);
+  $('span').show();
+  $('input').hide();
+
+  $("#field").mousemove(function (event) {
+    var coordsX = event.pageX + "";
+    var coordsY = event.pageY + "";
+
+    socket.emit('coords', coordsX, coordsY);
+  });
+});
+
+socket.on('get info', function () {
+
+  socket.emit('user', name, clientCursorColor, nameId, id);
+})
+
+socket.on('append user', function (name, color, idName) {
+  if (searchIDInList(idName)) {
+    $('#usersConnect').append($("<div>", {
+      "class": "names",
+      "id": idName
+    }).text(name).css('color', color));
+  }
 });
 
 socket.on('new coords', function (coordsX, coordsY, color, index) {
@@ -25,7 +55,17 @@ socket.on('new coords', function (coordsX, coordsY, color, index) {
   }).css(styles));
 });
 
-socket.on('close', function (index) {
-  console.log(index);
-  $('#' + index).remove();
+socket.on('close', function (cursor, name) {
+
+  ifAuth = false;
+  $('#' + cursor).remove();
+  $('#' + name).remove();
 });
+
+function searchIDInList(id) {
+  if (!usersConnect.includes(id)) {
+    usersConnect.push(id);
+    return true;
+  }
+  return false;
+}
--- sockets/main.go
+++ sockets/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/googollee/go-socket.io"
 )
@@ -13,21 +14,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	server.On("connection", func(so socketio.Socket) {
 		log.Println("on connection")
+		so.BroadcastTo("cursor field", "get info")
 
 		so.Join("cursor field")
-		i := ""
+		var idCursor, idName, userColor string
 
-		so.On("coords", func(coordsX, coordsY, color, id string) {
-			i = id
+		so.On("user", func(user, color, nameId, cursorId string) {
+			idName = nameId
+			idCursor = cursorId
+			userColor = color
+			if user != "" {
+				so.BroadcastTo("cursor field", "append user", user, color, nameId)
+			}
+		})
+
+		so.On("coords", func(coordsX, coordsY string) {
+
 			//circle client cursor
-			//so.Emit("new coords", coordsX, coordsY, color, id)
-			so.BroadcastTo("cursor field", "new coords", coordsX, coordsY, color, id)
+			so.Emit("new coords", coordsX, coordsY, userColor, idCursor)
+			so.BroadcastTo("cursor field", "new coords", coordsX, coordsY, userColor, idCursor)
 		})
 
 		so.On("disconnection", func() {
-			so.BroadcastTo("cursor field", "close", i)
+			so.BroadcastTo("cursor field", "close", idCursor, idName)
 			log.Println("on disconnect")
 		})
 	})
@@ -38,5 +50,5 @@ func main() {
 	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("./asset")))
 	log.Println("Serving at localhost:5000...")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
